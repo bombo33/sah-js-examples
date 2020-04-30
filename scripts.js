28779388df5c1fc977c142cab6bd971370e5ea11
bombo33 <pappbotond2001@gmail.com>
2020-04-30 17:31:00 +0200

    add jquery

--- scripts.js
+++ scripts.js
@@ -1,1 +1,8 @@
+//Third part
+
+$('h1').html('This is done by <em>JawaScript</em>!');
+$('p').css('border', '1px solid red');
+$('p').css('padding', '20px');
+$('h2').toggleClass('blue');
+
 console.log('Script file is loaded.');
