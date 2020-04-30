0a880f710b0c2b46179a7c0b0260b0e040be65bb
bombo33 <pappbotond2001@gmail.com>
2020-04-30 17:56:03 +0200

    add third part

--- scripts.js
+++ scripts.js
@@ -1,8 +1,21 @@
 //Third part
+console.log('Script file is loaded.');
 
 $('h1').html('This is done by <em>JawaScript</em>!');
-$('p').css('border', '1px solid red');
-$('p').css('padding', '20px');
 $('h2').toggleClass('blue');
 
-console.log('Script file is loaded.');
+
+
+
+function myLittleFunction() {
+    $('main').append('<p>A p is appended in main</p>')
+    $('p').css('border', '1px solid red');
+    $('p').css('padding', '10px');
+    
+}
+
+
+$('#add').click(myLittleFunction);
+$('#remove').click(function() {
+    $('p:last-child').remove();
+});
